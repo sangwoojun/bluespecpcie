c680982632b5237196e03476e2f4390a9850af28
Sang-Woo Jun <swjun@ics.uci.edu>
2018-10-01 18:36:30 -0700

    Benchmarking DRAM DMA

--- examples/sortreduce/cpp/main.cpp
+++ examples/sortreduce/cpp/main.cpp
@@ -19,14 +19,33 @@ int main(int argc, char** argv) {
 	uint8_t* dmabuf8 = (uint8_t*)pcie->dmaBuffer();
 	uint32_t* dmabuf32 = (uint32_t*)dmabuf8;
 
+	int writeStatOff = 256*4;
+	int readStatOff = 257*4;
+	int pageCnt = 1024*4;
+
+	uint32_t writeDoneCnt = pcie->userReadWord(writeStatOff);
+	uint32_t readDoneCnt = pcie->userReadWord(readStatOff);
 
 	for ( int i = 0; i < 1024*256; i++ ) { // 1MB
 		dmabuf32[i] = i;// | (0xcc<<24);
 	}
 
-	pcie->userWriteWord(256*4, 2); // host mem page 1 
-	pcie->userWriteWord(257*4, 4); // fpga mem page 2
-	pcie->userWriteWord(258*4, 32); // copy 32 KB host->fpga
+	timespec start;
+	timespec now;
+	double diff = 0;
+
+
+	clock_gettime(CLOCK_REALTIME, & start);
+	for ( int i = 0; i < pageCnt; i++ ) {
+		pcie->userWriteWord(256*4, 0); // host mem page 1 
+		pcie->userWriteWord(257*4, 4); // fpga mem page 2
+		pcie->userWriteWord(258*4, 64); // copy 256 Pages host->fpga
+	}
+
+	while ( writeDoneCnt + pageCnt > pcie->userReadWord(writeStatOff) );
+
+	clock_gettime(CLOCK_REALTIME, & now);
+	printf( "Elapsed: %lf\n", timespec_diff_sec(start,now));
 	
 	sleep(1);
 
@@ -34,9 +53,17 @@ int main(int argc, char** argv) {
 		dmabuf32[i] = 0;
 	}
 
-	pcie->userWriteWord(256*4, 0); // host mem page 1 
-	pcie->userWriteWord(257*4, 12); // fpga mem page 2
-	pcie->userWriteWord(259*4, 32); // copy 32 KB fpga->host
+	clock_gettime(CLOCK_REALTIME, & start);
+	for ( int i = 0; i < 1024*4; i++ ) {
+		pcie->userWriteWord(256*4, 0); // host mem page 1 
+		pcie->userWriteWord(257*4, 12); // fpga mem page 2
+		pcie->userWriteWord(259*4, 64); // copy 256 Pages fpga->host
+	}
+	
+	while ( readDoneCnt + pageCnt > pcie->userReadWord(readStatOff) );
+
+	clock_gettime(CLOCK_REALTIME, & now);
+	printf( "Elapsed: %lf\n", timespec_diff_sec(start,now));
 
 	sleep(1);
 
@@ -44,6 +71,7 @@ int main(int argc, char** argv) {
 		printf( "%x\n", dmabuf32[i]);
 	}
 
-	printf( "%x -- %x\n", pcie->userReadWord(256*4), pcie->userReadWord(257*4) );
+	printf( "%x -- %x\n", pcie->userReadWord(writeStatOff), pcie->userReadWord(readStatOff) );
+	//printf( "%x -- %x\n", pcie->userReadWord(256*4), pcie->userReadWord(257*4) );
 
 }
